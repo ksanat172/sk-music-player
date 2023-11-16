4e7977085de41c134ce69f4603b55133f133c774
Sanat Kumar <sanatkumar172@gmail.com>
2023-11-16 19:00:26 +0530

    Fix

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ wss.on('connection', function connection(ws) {
         })
     })
     wss.clients.forEach(function each(client) {
-        if (client !== ws && client.readyState === WebSocket.OPEN) {
+        if (client.readyState === WebSocket.OPEN) {
             client.send('Joined');
         }
     })
