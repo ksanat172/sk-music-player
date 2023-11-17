911fd6b5b254be42d93665ec45f0b4c7dc4f1d0a
Sanat Kumar <sanatkumar172@gmail.com>
2023-11-17 08:32:14 +0530

    test

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const WebSocket = require('ws');
 
 const port = 5000;
-
+let concatStr = "";
 const wss = new WebSocket.Server({
     port: port
 });
 
 wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(data) {
+        concatStr += data;
         const stringifiedData = data.toString('utf-8')
         wss.clients.forEach(function each(client) {
             if (client !== ws && client.readyState === WebSocket.OPEN) {
@@ -17,7 +18,7 @@ wss.on('connection', function connection(ws) {
     })
 
 
-    ws.send('Joined');
+    ws.send(concatStr);
 
 })
 
